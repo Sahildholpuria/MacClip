fe48fbec18d01110a192e539e3f2d6ab29e46bcb
sahildholpuria <dholpuria1999@gmail.com>
2026-09-12 13:26:55 +0530

    Bump version to 1.0.2 with verified auto-paste and DMG installer

--- build_app.sh
+++ build_app.sh
@@ -47,9 +47,9 @@ cat << 'PLIST' > "$CONTENTS_DIR/Info.plist"
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.1</string>
+    <string>1.0.2</string>
     <key>CFBundleVersion</key>
-    <string>2</string>
+    <string>3</string>
     <key>LSMinimumSystemVersion</key>
     <string>12.0</string>
     <key>LSUIElement</key>
--- create_dmg.sh
+++ create_dmg.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-VERSION="1.0.1"
+VERSION="1.0.2"
 DMG_NAME="MacClip-${VERSION}.dmg"
 DIST_DIR="$DIR/dist"
 STAGING_DIR="/tmp/macclip_dmg_staging"
